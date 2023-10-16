501dc576c693e9724de98556118a0b610ff21d74
Ayisha Taher <tahera@go.stockton.edu>
2023-10-16 13:24:58 -0400

    something = "Hello World"

--- Project1AT.py
+++ Project1AT.py
@@ -8,7 +8,7 @@
 # Quiz Game:
 # Create a simple console-based quiz game where the user answers a series of questions.
 # The game should keep track of the user's score and provide feedback based on the answers given.
-# points = 0
+points = 0
 user_name = input("What is your name? ")
 print("Hello " + user_name + "!")
 # Write a function that displays a welcome message to the user and explains the rules of the game
@@ -30,8 +30,27 @@ print(welcome_message())
 # Create a function to ask a question and check the answer. This function should accept parameters like the question, options, and the correct answer, and return whether the user was correct.
 # an example would be def ask_question(question, option_1, option_2, option_3, option_4, correct_answer):
 # the return value should be a boolean (True or False) for whether the user was correct
-
+def ask_question(question, option_1, option_2, option_3, option_4, correct_answer):
+    print(question)
+    print("A"+  option_1)
+    print("B" + option_2)
+    print("C" + option_3)
+    print("D" + option_4)
+    user_answer = input("What is your answer? ")
+    if user_answer == correct_answer:
+        return True
+        points = points + 1
+    else:
+        return False
+QuestionA = ask_question("What is Spongebob's pet's name?", "Gary", "Bob", "Greg", "Sandy", "A")
+QuestionB = ask_question("What is the name of the building that Spongebob works at?", "Chum Bucket", "Krusty Krab", "Krusty Chum", "Krusty Plankton", "B")
+QuestionC = ask_question("What is the name of Spongebob's best friend?", "Larry", "Sandy", "Patrick", "Squidward", "C")
+QuestionD = ask_question("What is the name of Spongebob's favorite food?", "Pizza", "Chum", "Kelp Fries", "Krabby Patty", "D")
 # Create a function to display the final score, which takes the score as a parameter and displays a message.
+def final_score(points): 
+    print("Your score is " + str(points))    
+print(final_score(points))
+
 # Loops:
 # Use a for or while loop to iterate through the questions.
 # Variable Casting:
